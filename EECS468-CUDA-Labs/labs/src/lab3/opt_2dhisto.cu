574f03585af7adad2e14bcf49290f28f9cbe4aac
caygen <caygen@u.northwestern.edu>
2017-02-19 22:22:36 -0600

    } fix

--- EECS468-CUDA-Labs/labs/src/lab3/opt_2dhisto.cu
+++ EECS468-CUDA-Labs/labs/src/lab3/opt_2dhisto.cu
@@ -43,9 +43,6 @@ __global__ void histoKernel(uint32_t *input, size_t height, size_t width, uint32
         globalTid += stride;
      }
 }
-
-
-}
 __global__ void opt_32to8Kernel(uint32_t *input, uint8_t* output, size_t length){
 	int idx = blockDim.x * blockIdx.x + threadIdx.x;
 	output[idx] = (uint8_t)((input[idx] < UINT8_MAX) * input[idx]) + (input[idx] >= UINT8_MAX) * UINT8_MAX;
