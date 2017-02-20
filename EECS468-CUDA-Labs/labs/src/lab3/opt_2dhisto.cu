71fd2bce99301ed70bd79087df955e8d989186ad
caygen <caygen@u.northwestern.edu>
2017-02-20 16:06:39 -0600

    saturate

--- EECS468-CUDA-Labs/labs/src/lab3/opt_2dhisto.cu
+++ EECS468-CUDA-Labs/labs/src/lab3/opt_2dhisto.cu
@@ -6,8 +6,11 @@
 #include "util.h"
 #include "ref_2dhisto.h"
 
-__global__ void histoKernel(uint32_t*, size_t, size_t, uint32_t*);
-__global__ void opt_32to8Kernel(uint32_t*, uint8_t*, size_t);
+//__global__ void histoKernel(uint32_t*, size_t, size_t, uint32_t*);
+//__global__ void opt_32to8Kernel(uint32_t*, uint8_t*, size_t);
+
+__global__ void histoKernel2(uint32_t *input, size_t height, uint32_t* bins);
+__global__ void saturate(unsigned int *bins, unsigned int num_bins);
 
 void opt_2dhisto(uint32_t* input, size_t height, size_t width, uint8_t* bins, uint32_t* g_bins)
 {
@@ -28,7 +31,7 @@ void opt_2dhisto(uint32_t* input, size_t height, size_t width, uint8_t* bins, ui
 }
 
 /* Include below the implementation of any other functions you need */
-
+/*
 __global__ void histoKernel(uint32_t *input, size_t height, size_t width, uint32_t* bins){
       int globalTid = blockDim.x * blockIdx.x + threadIdx.x;
      //__shared__ uint32_t* s_input = input;
@@ -43,18 +46,42 @@ __global__ void histoKernel(uint32_t *input, size_t height, size_t width, uint32
         globalTid += stride;
      }
 }
-/*
-__global__ void histoKernel2(uint32_t *input, size_t height, uint32_t* bins){
-  int globalTid = blockDim.x * blockIdx.x + threadIdx.x;
-  unsigned int num_elements = height * width;
-  __shared__ unsigned int s_bins[HISTO_HEIGHT*HISTO_WIDTH];
-}
-*/
 
 __global__ void opt_32to8Kernel(uint32_t *input, uint8_t* output, size_t length){
 	int idx = blockDim.x * blockIdx.x + threadIdx.x;
 	output[idx] = (uint8_t)((input[idx] < UINT8_MAX) * input[idx]) + (input[idx] >= UINT8_MAX) * UINT8_MAX;
 	__syncthreads();
+}
+*/
+
+__global__ void histoKernel2(uint32_t *input, size_t height, uint32_t* bins){
+  int globalTid = blockDim.x * blockIdx.x + threadIdx.x;
+  unsigned int num_elements = height * width;
+  __shared__ unsigned int s_bins[num_elements];
+  int stride = blockDim.x * gridDim.x;
+  if (threadIdx.x < num_bins) {
+		s_bins[threadIdx.x] = 0;
+	}
+  while (globalTid < num_elements){
+     int value = input[globalTid];
+     atomicAdd( &(s_bins[value]), 1);
+     globalTid += stride;
+  }
+  __syncthreads();
+
+  if (threadIdx.x < num_bins) {
+  		atomicAdd(&(bins[threadIdx.x]), s_bins[threadIdx.x]);
+  	}
+}
+
+__global__ void saturate(unsigned int *bins, unsigned int num_bins) {
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (i < num_bins) {
+		if (bins[i] > 255) {
+			bins[i] = 255;
+    }
+	}
 }
 
 void* AllocateOnDevice(size_t size){
