5a44a2718b3bccc6203e09e2f69936dcea2c3e9d
caygen <caygen@u.northwestern.edu>
2017-03-06 16:15:28 -0600

    did i break sth.?

--- EECS468-CUDA-Labs/labs/src/lab4/scan_largearray_kernel.cu
+++ EECS468-CUDA-Labs/labs/src/lab4/scan_largearray_kernel.cu
@@ -8,14 +8,15 @@
 #define NUM_BANKS 32
 #define LOG_NUM_BANKS 5
 // Lab4: You can use any other block size you wish.
-#define BLOCK_SIZE 1024 
-#define CONFLICT_FREE_OFFSET(n) \  
-    ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS)) 
+#define BLOCK_SIZE 1024
+#define CONFLICT_FREE_OFFSET(n) \
+    ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))
 
 __global__ void scan(float *g_odata, float *g_idata, int blockSize);
 __global__ void copy(float *from_scanned,float *from_unscanned, float *to, int numCopies);
 __global__ void addArray(float *from, float *to, int divisor, int numElements);
 
+__global__ void scan2(float *g_odata, float *g_idata, int n);
 
 // **===-------- Lab4: Modify the body of this function -----------===**
 // You may need to make multiple kernel calls, make your own kernel
@@ -32,13 +33,15 @@ void prescanArray(float *outArray, float *inArray, int numElements,
 
 	// do scan on array1, write back to array2
 	scan<<<16, 512>>>(array2, array1, 1024);
+  //scan2<<<16, 512>>>(array2, array1, 1024);
 
 	// read every 1024 elements from array2 and write it to array3
-	copy<<<1, 16>>>(array2, array1, array3, 16); 
+	copy<<<1, 16>>>(array2, array1, array3, 16);
 
 	// do scan on array3, write back to array4
-	scan<<<1, 8>>>(array4, array3, 16);	
-	
+	scan<<<1, 8>>>(array4, array3, 16);
+	//scan2<<<1, 8>>>(array4, array3, 16);
+
 	// add array2[i] to outArray[i*1048576:((i+1)*1048576)-1]
 	addArray<<<16384, 1024>>>(array4, outArray, 1024*1024, numElements);
 
@@ -49,10 +52,10 @@ void prescanArray(float *outArray, float *inArray, int numElements,
 // **===-----------------------------------------------------------===**
 
 
-// read every 1024 elements from from, and write it to to 
+// read every 1024 elements from from, and write it to to
 __global__ void copy(float *from_scanned, float *from_unscanned, float *to, int numCopies)
 {
-	int thid = blockIdx.x * blockDim.x + threadIdx.x;  
+	int thid = blockIdx.x * blockDim.x + threadIdx.x;
 	int numThreads = gridDim.x * blockDim.x;
 	//if (thid < numCopies)
 	for (int i = thid; i < numCopies; i += numThreads)
@@ -62,49 +65,49 @@ __global__ void copy(float *from_scanned, float *from_unscanned, float *to, int 
 	}
 }
 
-// do scan on g_idata, write back to g_odata 
+// do scan on g_idata, write back to g_odata
 __global__ void scan(float *g_odata, float *g_idata, int blockSize)
 {
 	__shared__ float temp[1024];
-	int allid = blockIdx.x * blockDim.x + threadIdx.x;  
-	int thid = threadIdx.x;  
+	int allid = blockIdx.x * blockDim.x + threadIdx.x;
+	int thid = threadIdx.x;
 	int offset = 1;
 
-	temp[2*thid] = g_idata[2*allid]; // load input into shared memory  
-	temp[2*thid+1] = g_idata[2*allid+1];  
+	temp[2*thid] = g_idata[2*allid]; // load input into shared memory
+	temp[2*thid+1] = g_idata[2*allid+1];
 
-	for (int d = blockSize>>1; d > 0; d >>= 1)    // build sum in place up the tree  
-	{   
-		__syncthreads();  
-   		if (thid < d)  
-		{  
-			int ai = offset*(2*thid+1)-1;  
-			int bi = offset*(2*thid+2)-1;  
-			temp[bi] += temp[ai];  
+	for (int d = blockSize>>1; d > 0; d >>= 1)    // build sum in place up the tree
+	{
+		__syncthreads();
+   		if (thid < d)
+		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
+			temp[bi] += temp[ai];
 		}
 		offset *= 2;
 	}
 
-	if (thid == 0) { temp[blockSize - 1] = 0; } // clear the last element  
+	if (thid == 0) { temp[blockSize - 1] = 0; } // clear the last element
 
-	for (int d = 1; d < blockSize; d *= 2) // traverse down tree & build scan  
-	{  
-     		offset >>= 1;  
-     		__syncthreads();  
-     		if (thid < d)                       
-     		{	
-			int ai = offset*(2*thid+1)-1;  
-			int bi = offset*(2*thid+2)-1;  
+	for (int d = 1; d < blockSize; d *= 2) // traverse down tree & build scan
+	{
+     		offset >>= 1;
+     		__syncthreads();
+     		if (thid < d)
+     		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
 
-			float t = temp[ai];  
-			temp[ai] = temp[bi];  
-			temp[bi] += t;   
+			float t = temp[ai];
+			temp[ai] = temp[bi];
+			temp[bi] += t;
 		}
-	}  
+	}
  	__syncthreads();
 
-	g_odata[2*allid] = temp[2*thid]; // write results to device memory  
-	g_odata[2*allid+1] = temp[2*thid+1];  
+	g_odata[2*allid] = temp[2*thid]; // write results to device memory
+	g_odata[2*allid+1] = temp[2*thid+1];
 }
 
 // add from[i] to to[i*divisor:((i+1)*divisor)-1]
@@ -118,59 +121,60 @@ __global__ void addArray(float *from, float *to, int divisor, int numElements)
 	}
 }
 
-/*
+
+
 // below is scan optimized for bank conflicts
-__global__ void scan(float *g_odata, float *g_idata, int n)  
-{  
+__global__ void scan2(float *g_odata, float *g_idata, int n)
+{
 	__shared__ float temp[1024];
-	int thid = blockIdx.x * blockDim.x + threadIdx.x;  
+	int thid = blockIdx.x * blockDim.x + threadIdx.x;
 	int offset = 1;
 
-	int ai = thid;  
-	int bi = thid + (n/2);  
+	int ai = thid;
+	int bi = thid + (n/2);
 	int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
-	int bankOffsetB = CONFLICT_FREE_OFFSET(bi); 
-	temp[ai + bankOffsetA] = g_idata[ai]; 
+	int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
+	temp[ai + bankOffsetA] = g_idata[ai];
 	temp[bi + bankOffsetB] = g_idata[bi];  // load input into shared memory
-	  
 
-	for (int d = n>>1; d > 0; d >>= 1)    // build sum in place up the tree  
-	{   
-		__syncthreads();  
-   		if (thid < d)  
-		{  
-			int ai = offset*(2*thid+1)-1;  
-			int bi = offset*(2*thid+2)-1;  
+
+	for (int d = n>>1; d > 0; d >>= 1)    // build sum in place up the tree
+	{
+		__syncthreads();
+   		if (thid < d)
+		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
 			ai += CONFLICT_FREE_OFFSET(ai);
 			bi += CONFLICT_FREE_OFFSET(bi);
 
-			temp[bi] += temp[ai];  
+			temp[bi] += temp[ai];
 		}
 		offset *= 2;
 	}
 
-	if (thid==0) { temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)] = 0;}  // clear the last element  
+	if (thid==0) { temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)] = 0;}  // clear the last element
 
-	for (int d = 1; d < n; d *= 2) // traverse down tree & build scan  
-	{  
-     		offset >>= 1;  
-     		__syncthreads();  
-     		if (thid < d)                       
-     		{	
-			int ai = offset*(2*thid+1)-1;  
-			int bi = offset*(2*thid+2)-1;  
+	for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
+	{
+     		offset >>= 1;
+     		__syncthreads();
+     		if (thid < d)
+     		{
+			int ai = offset*(2*thid+1)-1;
+			int bi = offset*(2*thid+2)-1;
 			ai += CONFLICT_FREE_OFFSET(ai);
 			bi += CONFLICT_FREE_OFFSET(bi);
 
-			float t = temp[ai];  
-			temp[ai] = temp[bi];  
-			temp[bi] += t;   
+			float t = temp[ai];
+			temp[ai] = temp[bi];
+			temp[bi] += t;
 		}
-	}  
+	}
  	__syncthreads();
 
-	g_odata[ai] = temp[ai + bankOffsetA];  
-	g_odata[bi] = temp[bi + bankOffsetB];  // write results to device memory  
+	g_odata[ai] = temp[ai + bankOffsetA];
+	g_odata[bi] = temp[bi + bankOffsetB];  // write results to device memory
 }
-*/
+
 #endif // _PRESCAN_CU_
